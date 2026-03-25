8b9cfb7c604acd5fb520184853e4c5fea0351ec9
Jan Radziszowski <255682@edu.p.lodz.pl>
2026-03-25 11:39:49 +0100

    8. Użycie adnotacji @Override tam, gdzie to możliwe

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,8 +2,9 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+    public final defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
 
+    @Override
     public int countOut() {
         while (!callCheck())
             temp.countIn(super.countOut());
