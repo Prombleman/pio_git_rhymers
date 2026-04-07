cac60f002e31a5701ff1988da0802534b235abc3
Artsiom Pratasevich <https.artempro@gmail.com>
2026-04-07 20:12:26 +0300

    17. Wygenerowanie i uzupełnienie dokumentacji Javadoc

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,10 +1,21 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa reprezentująca wyliczankę opartą na zasadach wieży Hanoi.
+ * Odrzuca liczby większe od poprzednio dodanej.
+ */
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
+    /**
+     * Stała reprezentująca początkową liczbę odrzuconych prób.
+     */
     public static final int INITIAL_REJECTED = 0;
     private int totalRejected = INITIAL_REJECTED;
 
+    /**
+     * Metoda zwracająca całkowitą liczbę odrzuconych elementów.
+     * * @return liczba odrzuconych elementów (błędnych prób dodania)
+     */
     public int reportRejected() {
         return totalRejected;
     }
